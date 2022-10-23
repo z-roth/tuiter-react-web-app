992bffefc58c7acee8571072b6bfbb4d36079cb8
Zack Roth <zack27r@gmail.com>
2022-10-23 17:36:31 -0400

    Push again

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,7 +1,16 @@
+import Nav from "../../Nav";
+import Classes from "./classes";
+import ConditionalOutput from "./conditional-output";
+import Styles from "./styles";
+
 function Assignment6() {
   return (
     <div>
+      <Nav />
       <h1>Assignment 6</h1>
+      <ConditionalOutput />
+      <Styles />
+      <Classes />
     </div>
   );
 }
---
+++ src/tuiter/index.js
@@ -0,0 +1,13 @@
+import Nav from "../Nav";
+import NavigationSidebar from "./navigation-sidebar";
+
+function Tuiter() {
+  return (
+    <div>
+      <Nav />
+      <NavigationSidebar active="home" />
+      <h1>Tuiter</h1>
+    </div>
+  );
+}
+export default Tuiter;
